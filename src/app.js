e03be93b2a52a37822ee3d2e58269181b84bd00c
Shah Rukh <shah.xhtml@yahoo.com>
2021-09-01 02:58:34 +0530

    change port case

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const { dir } = require('console')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // Path Definitions
 const viewsPath = path.join(__dirname, '../hbs-templates/views')
